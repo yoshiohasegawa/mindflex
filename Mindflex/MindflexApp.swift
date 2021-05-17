dbca3d8ac926e7927a63f6198c37b9019a5853e3
yoshiohasegawa <yoshio.seisuke.hasegawa@gmail.com>
2021-05-17 12:35:49 +0900

    Initial Commit

---
+++ Mindflex/MindflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindflexApp.swift
+//  Mindflex
+//
+//  Created by Yoshio Hasegawa on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct MindflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
